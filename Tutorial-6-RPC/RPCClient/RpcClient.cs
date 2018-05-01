d92934f3f0d2693b2eb2c0864d1c75ec5f3cd63e
José González <maangx@gmail.com>
2018-05-01 14:32:40 -0300

    Tutorial 6: RPC

---
+++ Tutorial-6-RPC/RPCClient/RpcClient.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Tutoriales_RabbitMQ.Tutorial_6.RPCClient
+{
+    class RpcClient
+    {
+        private readonly IConnection connection;
+        private readonly IModel channel;
+        private readonly string replyQueueName;
+        private readonly EventingBasicConsumer consumer;
+        private readonly BlockingCollection<string> respQueue = new BlockingCollection<string>();
+        private readonly IBasicProperties props;
+
+        public RpcClient()
+        {
+            RabbitMQConfig config;
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\rabbitmq.config.json";
+            
+            try{
+                config = JsonConvert.DeserializeObject<RabbitMQConfig>(File.ReadAllText(path));
+            } catch {
+                Console.WriteLine("# Error: Archivo de configuración no encontrado.");
+                return;
+            }
+
+            var factory = new ConnectionFactory() { HostName =  config.Hostname, 
+                                                    UserName = config.Username, 
+                                                    VirtualHost = config.VHost, 
+                                                    Password = config.Password };
+            try
+            {
+                connection = factory.CreateConnection();
+                channel = connection.CreateModel();
+                replyQueueName = channel.QueueDeclare().QueueName;
+                var correlationId = Guid.NewGuid().ToString();
+                consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) => {
+                    var body = ea.Body;
+                    var response = Encoding.UTF8.GetString(body);
+
+                    if (ea.BasicProperties.CorrelationId == correlationId)
+                    {
+                        respQueue.Add(response);
+                    }
+                };
+
+                props = channel.CreateBasicProperties();
+                props.ReplyTo = replyQueueName;
+                props.CorrelationId = correlationId;
+            } catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException) {
+                Console.WriteLine("# Error: No pudo establecerse una conexión con el servidor RabbitMQ. Finalizando la aplicación.");
+            }
+        }
+
+        public string Call(string message)
+        {
+            var messageBytes = Encoding.UTF8.GetBytes(message);
+            channel.BasicPublish(
+                exchange: "",
+                routingKey: "rpc_queue",
+                basicProperties: props,
+                body: messageBytes
+            );
+
+            channel.BasicConsume(
+                consumer: consumer,
+                queue: replyQueueName,
+                autoAck: true
+            );
+
+            return respQueue.Take();
+        }
+
+        public void Close()
+        {
+            connection.Close();
+            connection.Dispose();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rpcClient = new RpcClient();
+
+            Console.WriteLine("<== Solicitando fib(30)");
+            var response = rpcClient.Call("30");
+
+            Console.WriteLine("==> Recibido  '{0}'", response);
+            rpcClient.Close();
+        }
+    }
+}
---
+++ Tutorial-6-RPC/RPCWorker/RpcWorker.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Tutoriales_RabbitMQ.Tutorial_6.RPCWorker
+{
+    class RpcWorker
+    {
+        static void Main(string[] args)
+        {
+            RabbitMQConfig config;
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\rabbitmq.config.json";
+            
+            try{
+                config = JsonConvert.DeserializeObject<RabbitMQConfig>(File.ReadAllText(path));
+            } catch {
+                Console.WriteLine("# Error: Archivo de configuración no encontrado.");
+                return;
+            }
+
+            var factory = new ConnectionFactory() { HostName =  config.Hostname, 
+                                                    UserName = config.Username, 
+                                                    VirtualHost = config.VHost, 
+                                                    Password = config.Password };
+            try 
+            {
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare(queue: "rpc_queue",
+                                            durable: false,
+                                            exclusive: false,
+                                            autoDelete: false,
+                                            arguments: null);
+                        channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+                        var consumer = new EventingBasicConsumer(channel);
+                        consumer.Received += (model, ea) => {
+                            string response = null;
+
+                            var body = ea.Body;
+                            var props = ea.BasicProperties;
+                            var replyProps = channel.CreateBasicProperties();
+                            replyProps.CorrelationId = props.CorrelationId;
+
+                            try
+                            {
+                                var message = Encoding.UTF8.GetString(body);
+                                int n = int.Parse(message);
+                                Console.WriteLine("==> Calculando fib({0})", message);
+                                response = fib(n).ToString();
+                            } catch (Exception ex) {
+                                Console.WriteLine("Error: " + ex.Message);
+                                response = "";
+                            } finally {
+                                var responseBytes = Encoding.UTF8.GetBytes(response);
+                                channel.BasicPublish(
+                                    exchange: "",
+                                    routingKey: props.ReplyTo,
+                                    basicProperties: replyProps,
+                                    body: responseBytes
+                                );
+                                channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                            }
+                        };
+
+
+                        channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+                        Console.WriteLine("Esperando solicitudes RPC... Presiona [Enter] para finalizar.");
+                        Console.ReadLine();
+                    }
+                }
+            } catch (RabbitMQ.Client.Exceptions.BrokerUnreachableException) {
+                Console.WriteLine("# Error: No pudo establecerse una conexión con el servidor RabbitMQ. Finalizando la aplicación.");
+            }
+        }
+
+        private static int fib(int n)
+        {
+            if (n == 0 || n == 1)
+                return n;
+            
+            return fib(n - 1) + fib(n - 2);
+        }
+    }
+}
